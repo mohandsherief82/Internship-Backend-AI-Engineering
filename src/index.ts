446ba140fc415604140693354226e7d4f62ffd2e
mohandsheried82 <219840617+mohandsherief82@users.noreply.github.com>
2026-07-16 19:48:39 +0300

    Added some comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express, {type Express, type Request, type Response} from 'express'
 
+// Initializing the server
 const app: Express = express();
 const port: number = 3000;
 
 app.use(express.json());
 
+// Root route handler
 app.get('/', (req: Request, res: Response) => {
     res.status(200).json({message: "Hello World!!\n"});
 });
 
+// Bind app to port and starts event loop
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
