95cae5dc4b6e430c18903efdf4ee6babfb23dcec
mohandsheried82 <219840617+mohandsherief82@users.noreply.github.com>
2026-07-17 08:32:48 +0300

    Created the tasks POST route handler with validation

--- src/index.ts
+++ src/index.ts
@@ -58,6 +58,27 @@ app.get('/tasks/:id', (req: Request<{id: string}>, res: Response) => {
               .json(foundTask);
 });
 
+app.post('/tasks', (req: Request<{}, {}, Omit<Task, 'id' | 'done'>>, res: Response) => {
+    if (!req.body) {
+        return res.status(400).json({ error: "Invalid or missing JSON payload." });
+    }
+    
+    const { title } = req.body;
+
+    if (!title || typeof title !== 'string') {
+        return res.status(400)
+                  .json("Missing or Empty task title.");
+    }
+
+    const nextID = tasks.length > 0 ? Math.max(...tasks.map(t => t.id)) + 1 : 0;
+
+    const newTask: Task = {id: nextID, title: title, done: false};
+
+    tasks.push(newTask);
+
+    return res.status(201).json(newTask);
+});
+
 // Bind app to port and starts event loop
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
