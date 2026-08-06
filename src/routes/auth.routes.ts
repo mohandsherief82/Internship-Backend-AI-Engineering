26214cdaf3b4c0048aca3f7f1d6d3250b590124c
mohandsheried82 <219840617+mohandsherief82@users.noreply.github.com>
2026-08-06 21:10:15 +0300

    Finished the sign up path

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,13 +2,13 @@ import { Router, Request, Response, NextFunction } from 'express';
 
 import { validateEmail, validatePassword } from '../services/auth.services.js';
 import { Task } from '../db/database.js';
-import { ValidationError, NotFoundError } from '../errors.js';
+import { AuthError } from '../errors.js';
+import supabase from "../lib/supabase.js";
 
 const router = Router();
 
 router.post('/auth/signup', async (req: Request<{email: string, password: string}>, res: Response, next: NextFunction) => {
-    const email = req.params.email;
-    const pass = req.params.password;
+    const { email, pass } = req.body;
 
     try {
         validateEmail(email);
@@ -16,8 +16,24 @@ router.post('/auth/signup', async (req: Request<{email: string, password: string
         validatePassword(pass);
     } catch (err) {
         next(err);
+        return;
     }
 
+    const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: pass
+    });
+
+    if (error) {
+        const customError = new AuthError(error.message, error.status || 400, error.code);
+        next(customError);
+        return;
+    }
+
+    return res.status(201).json({
+        msg: "Created",
+        data: data
+    });
 });
 
 router.post('/auth/login', (req: Request, res: Response) => {
