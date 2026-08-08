24ad7d56e5b6410703677900ed3f73adab60c537
mohandsheried82 <219840617+mohandsherief82@users.noreply.github.com>
2026-08-08 19:53:38 +0300

    Added the public endpoint

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import swaggerDocument from "../openapi.json" with { type: 'json' };
 import tasksRouter from "./routes/tasks.routes.js";
 import metaRouter from "./routes/meta.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import gatesRouter from "./routes/gates.routes.js";
 import config from "./config/env.js";
 
 import express, { type Express } from 'express';
@@ -19,6 +20,7 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Use the paths
 app.use('/', metaRouter);
+app.use('/', gatesRouter);
 app.use('/', authRouter);
 app.use('/', tasksRouter);
 
